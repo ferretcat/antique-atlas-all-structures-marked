dae2aedd45e63f744c1b37ad4756b09e43e0d4ee
ferretcat <194603164+ferretcat@users.noreply.github.com>
2025-06-19 14:27:26 -0700

    Auto mark all found structures

--- src/main/java/folk/sisby/antique_atlas/reloader/MarkerTextures.java
+++ src/main/java/folk/sisby/antique_atlas/reloader/MarkerTextures.java
@@ -1,7 +1,13 @@
 package folk.sisby.antique_atlas.reloader;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+
 import folk.sisby.antique_atlas.AntiqueAtlas;
 import folk.sisby.antique_atlas.MarkerTexture;
 import folk.sisby.antique_atlas.util.CodecUtil;
@@ -15,112 +21,137 @@ import net.minecraft.resource.metadata.ResourceMetadataReader;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.profiler.Profiler;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+public class MarkerTextures extends SinglePreparationResourceReloader<Map<Identifier, MarkerTextures.MarkerTextureMeta>>
+        implements IdentifiableResourceReloadListener {
+    private static final MarkerTextures INSTANCE = new MarkerTextures();
+    public static final Identifier ID = AntiqueAtlas.id("marker_textures");
 
-public class MarkerTextures extends SinglePreparationResourceReloader<Map<Identifier, MarkerTextures.MarkerTextureMeta>> implements IdentifiableResourceReloadListener {
-	private static final MarkerTextures INSTANCE = new MarkerTextures();
-	public static final Identifier ID = AntiqueAtlas.id("marker_textures");
+    public static Identifier lol;
+    public static MarkerTexture lolol;
 
-	public static MarkerTextures getInstance() {
-		return INSTANCE;
-	}
+    public static MarkerTextures getInstance() {
+        return INSTANCE;
+    }
 
-	private final Map<Identifier, MarkerTexture> textures = new HashMap<>();
+    private final Map<Identifier, MarkerTexture> textures = new HashMap<>();
 
-	public MarkerTexture get(Identifier id) {
-		return textures.get(id);
-	}
+    public MarkerTexture get(Identifier id) {
+        return textures.get(id);
+    }
 
-	public MarkerTexture getOrDefault(Identifier id) {
-		return getOrDefault(id, MarkerTexture.DEFAULT);
-	}
+    public MarkerTexture getOrDefault(Identifier id) {
+        return getOrDefault(id, MarkerTexture.DEFAULT);
+    }
 
-	public MarkerTexture getOrDefault(Identifier id, MarkerTexture defaultTexture) {
-		return textures.getOrDefault(id, defaultTexture);
-	}
+    public MarkerTexture getOrDefault(Identifier id, MarkerTexture defaultTexture) {
+        return textures.getOrDefault(id, defaultTexture);
+    }
 
-	public MarkerTexture getLandmarkType(LandmarkType<?> type) {
-		return getOrDefault(Identifier.of(type.id().getNamespace(), "landmark/type/" + type.id().getPath()));
-	}
+    public MarkerTexture getLandmarkType(LandmarkType<?> type) {
+        return getOrDefault(Identifier.of(type.id().getNamespace(), "landmark/type/" + type.id().getPath()));
+    }
 
-	public MarkerTexture getLandmarkType(LandmarkType<?> type, String variant) {
-		return getOrDefault(Identifier.of(type.id().getNamespace(), "landmark/type/" + type.id().getPath() + (variant == null ? "" : "/" + variant)));
-	}
+    public MarkerTexture getLandmarkType(LandmarkType<?> type, String variant) {
+        return getOrDefault(Identifier.of(type.id().getNamespace(),
+                "landmark/type/" + type.id().getPath() + (variant == null ? "" : "/" + variant)));
+    }
 
-	public Map<Identifier, MarkerTexture> asMap() {
-		return new HashMap<>(textures);
-	}
+    public Map<Identifier, MarkerTexture> asMap() {
+        return new HashMap<>(textures);
+    }
 
-	@Override
-	protected Map<Identifier, MarkerTextureMeta> prepare(ResourceManager manager, Profiler profiler) {
-		Map<Identifier, MarkerTextures.MarkerTextureMeta> textureMeta = new HashMap<>();
-		for (Map.Entry<Identifier, Resource> e : manager.findResources("textures/atlas/marker", id -> id.getPath().endsWith(".png")).entrySet()) {
-			Identifier id = Identifier.of(e.getKey().getNamespace(), e.getKey().getPath().substring("textures/atlas/marker/".length(), e.getKey().getPath().length() - ".png".length()));
-			try {
-				ResourceMetadata metadata = e.getValue().getMetadata();
-				textureMeta.put(id, metadata.decode(MarkerTextures.MarkerTextureMeta.METADATA).orElse(MarkerTextureMeta.DEFAULT));
-			} catch (IOException ex) {
-				AntiqueAtlas.LOGGER.error("[Antique Atlas] Failed to access marker texture metadata for {}", e.getKey(), ex);
-				textureMeta.put(id, MarkerTextures.MarkerTextureMeta.DEFAULT);
-			}
-		}
-		return textureMeta;
-	}
+    @Override
+    protected Map<Identifier, MarkerTextureMeta> prepare(ResourceManager manager, Profiler profiler) {
+        Map<Identifier, MarkerTextures.MarkerTextureMeta> textureMeta = new HashMap<>();
+        for (Map.Entry<Identifier, Resource> e : manager
+                .findResources("textures/atlas/marker", id -> id.getPath().endsWith(".png")).entrySet()) {
+            String path = e.getKey().getPath().substring("textures/atlas/marker/".length(),
+                    e.getKey().getPath().length() - ".png".length());
+            Identifier id = Identifier.of(e.getKey().getNamespace(), path);
 
-	@Override
-	protected void apply(Map<Identifier, MarkerTextureMeta> prepared, ResourceManager manager, Profiler profiler) {
-		AntiqueAtlas.LOGGER.info("[Antique Atlas] Reloading Marker Textures...");
-		textures.clear();
-		prepared.forEach((id, meta) -> {
-			if (id.getPath().endsWith("_accent")) {
-				Identifier mainId = id.withPath(s -> s.substring(0, s.length() - "_accent".length()));
-				MarkerTextureMeta main = prepared.get(mainId);
-				if (main != null) {
-					textures.put(mainId, main.build(mainId, true));
-				} else {
-					AntiqueAtlas.LOGGER.error("[Antique Atlas] Marker accent {} has no main texture! Discarding.", id);
-				}
-			}
-		});
-		prepared.forEach((id, meta) -> {
-			if (!textures.containsKey(id) && !id.getPath().endsWith("_accent")) {
-				textures.put(id, meta.build(id, false));
-			}
-		});
-	}
+            if (path.equals("custom/point")) {
+                int textureWidth = 32;
+                int textureHeight = 32;
+                int mipLevels = 0;
+                int offsetX = -textureWidth / 2;
+                int offsetY = -textureHeight / 2;
+                int nearClip = 1;
+                int farClip = Integer.MAX_VALUE;
+                lolol = MarkerTexture.ofId(id, offsetX, offsetY, textureWidth, textureHeight, mipLevels, nearClip,
+                        farClip, false);
+                lol = id;
+            }
 
-	@Override
-	public Identifier getFabricId() {
-		return ID;
-	}
+            try {
+                ResourceMetadata metadata = e.getValue().getMetadata();
+                textureMeta.put(id,
+                        metadata.decode(MarkerTextures.MarkerTextureMeta.METADATA).orElse(MarkerTextureMeta.DEFAULT));
+            } catch (IOException ex) {
+                AntiqueAtlas.LOGGER.error("[Antique Atlas] Failed to access marker texture metadata for {}", e.getKey(),
+                        ex);
+                textureMeta.put(id, MarkerTextures.MarkerTextureMeta.DEFAULT);
+            }
+        }
 
-	public record MarkerTextureMeta(Optional<Integer> textureWidth, Optional<Integer> textureHeight, Optional<Integer> mipLevels, Optional<Integer> offsetX, Optional<Integer> offsetY, Optional<Integer> nearClip, Optional<Integer> farClip) {
-		public static final MarkerTextureMeta DEFAULT = new MarkerTextureMeta(Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty());
+        return textureMeta;
+    }
 
-		public static final Codec<MarkerTextureMeta> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-			Codec.INT.optionalFieldOf("textureWidth").forGetter(MarkerTextureMeta::textureWidth),
-			Codec.INT.optionalFieldOf("textureHeight").forGetter(MarkerTextureMeta::textureHeight),
-			Codec.INT.optionalFieldOf("mipLevels").forGetter(MarkerTextureMeta::mipLevels),
-			Codec.INT.optionalFieldOf("offsetX").forGetter(MarkerTextureMeta::offsetX),
-			Codec.INT.optionalFieldOf("offsetY").forGetter(MarkerTextureMeta::offsetY),
-			Codec.INT.optionalFieldOf("nearClip").forGetter(MarkerTextureMeta::nearClip),
-			Codec.INT.optionalFieldOf("farClip").forGetter(MarkerTextureMeta::farClip)
-		).apply(instance, MarkerTextureMeta::new));
+    @Override
+    protected void apply(Map<Identifier, MarkerTextureMeta> prepared, ResourceManager manager, Profiler profiler) {
+        AntiqueAtlas.LOGGER.info("[Antique Atlas] Reloading Marker Textures...");
+        textures.clear();
+        prepared.forEach((id, meta) -> {
+            if (id.getPath().endsWith("_accent")) {
+                Identifier mainId = id.withPath(s -> s.substring(0, s.length() - "_accent".length()));
+                MarkerTextureMeta main = prepared.get(mainId);
+                if (main != null) {
+                    textures.put(mainId, main.build(mainId, true));
+                } else {
+                    AntiqueAtlas.LOGGER.error("[Antique Atlas] Marker accent {} has no main texture! Discarding.", id);
+                }
+            }
+        });
+        prepared.forEach((id, meta) -> {
+            if (!textures.containsKey(id) && !id.getPath().endsWith("_accent")) {
+                textures.put(id, meta.build(id, false));
+            }
+        });
+    }
 
-		public static final ResourceMetadataReader<MarkerTextureMeta> METADATA = new CodecUtil.CodecResourceMetadataSerializer<>(CODEC, AntiqueAtlas.id("marker"));
+    @Override
+    public Identifier getFabricId() {
+        return ID;
+    }
 
-		public MarkerTexture build(Identifier id, boolean accent) {
-			int textureWidth = this.textureWidth.orElse(32);
-			int textureHeight = this.textureHeight.orElse(32);
-			int mipLevels = this.mipLevels.orElse(0);
-			int offsetX = this.offsetX.orElse(-textureWidth / 2);
-			int offsetY = this.offsetY.orElse(-textureHeight / 2);
-			int nearClip = this.nearClip.orElse(1);
-			int farClip = this.farClip.orElse(Integer.MAX_VALUE);
-			return MarkerTexture.ofId(id, offsetX, offsetY, textureWidth, textureHeight, mipLevels, nearClip, farClip, accent);
-		}
-	}
+    public record MarkerTextureMeta(Optional<Integer> textureWidth, Optional<Integer> textureHeight,
+            Optional<Integer> mipLevels, Optional<Integer> offsetX, Optional<Integer> offsetY,
+            Optional<Integer> nearClip, Optional<Integer> farClip) {
+        public static final MarkerTextureMeta DEFAULT = new MarkerTextureMeta(Optional.empty(), Optional.empty(),
+                Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty());
+
+        public static final Codec<MarkerTextureMeta> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+                Codec.INT.optionalFieldOf("textureWidth").forGetter(MarkerTextureMeta::textureWidth),
+                Codec.INT.optionalFieldOf("textureHeight").forGetter(MarkerTextureMeta::textureHeight),
+                Codec.INT.optionalFieldOf("mipLevels").forGetter(MarkerTextureMeta::mipLevels),
+                Codec.INT.optionalFieldOf("offsetX").forGetter(MarkerTextureMeta::offsetX),
+                Codec.INT.optionalFieldOf("offsetY").forGetter(MarkerTextureMeta::offsetY),
+                Codec.INT.optionalFieldOf("nearClip").forGetter(MarkerTextureMeta::nearClip),
+                Codec.INT.optionalFieldOf("farClip").forGetter(MarkerTextureMeta::farClip))
+                .apply(instance, MarkerTextureMeta::new));
+
+        public static final ResourceMetadataReader<MarkerTextureMeta> METADATA = new CodecUtil.CodecResourceMetadataSerializer<>(
+                CODEC, AntiqueAtlas.id("marker"));
+
+        public MarkerTexture build(Identifier id, boolean accent) {
+            int textureWidth = this.textureWidth.orElse(32);
+            int textureHeight = this.textureHeight.orElse(32);
+            int mipLevels = this.mipLevels.orElse(0);
+            int offsetX = this.offsetX.orElse(-textureWidth / 2);
+            int offsetY = this.offsetY.orElse(-textureHeight / 2);
+            int nearClip = this.nearClip.orElse(1);
+            int farClip = this.farClip.orElse(Integer.MAX_VALUE);
+            return MarkerTexture.ofId(id, offsetX, offsetY, textureWidth, textureHeight, mipLevels, nearClip, farClip,
+                    accent);
+        }
+    }
 }
